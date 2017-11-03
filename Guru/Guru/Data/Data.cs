767c202f4edc9035dc846bcbda3d2ec75f239cbc
thanoskapsalis <thanoskapsalis97@gmail.com>
2017-11-03 22:36:16 +0200

    First Commit

---
+++ Guru/Guru/Data/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru.Data
+{
+    public class Phonebook
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string PNumber { get; set; }
+
+
+           
+
+
+    }
+}
+
---
+++ Guru/Guru/Views/MyContacts.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Guru.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Guru
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyContacts : ContentPage
+    {
+      //  ObservableCollection<Phonebook> ContactList = new ObservableCollection<Phonebook>();
+      
+
+        public MyContacts()
+        {
+            InitializeComponent();
+            //ContactList.Add(new Phonebook("Thanos", "Kapsalis", "6945181568"));
+            ContactList.ItemsSource = App.ContactList;
+        }
+
+
+    }   
+}
